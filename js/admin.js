1366054f6dd638ce1dcd31b754268029fa7a0eff
Radhey <radheyborntorock@gmail.com>
2016-03-04 14:48:29 +0530

    Admin Updated

--- js/admin.js
+++ js/admin.js
@@ -41,23 +41,18 @@
 	    }
 	    
 	//Message Counting 
-	var messages=new Firebase("https://r3chat.firebaseio.com/users/messages");
-		console.log(messages);
+	var messages=new Firebase("https://r3chat.firebaseio.com/messages");
 		messages.on("value",function(serverMessages)
 			{
-				console.log("We are in");
+				
 				var count=0;
 				var msg=serverMessages.val();
-				//$("#messages").html("");
-				console.log();
 				$.each(msg, function(index, value)
 				{
-					console.Log("We are in each loop");
 					count+=1;
 				});
 				$("#total_msg").html(count);
-
-					console.log("We are out of each loop");
+					
 			});
 
 /*Logout Function*/
